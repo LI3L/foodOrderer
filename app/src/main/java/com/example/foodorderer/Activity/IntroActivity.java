6e23961eafd068f08dc6df5ec5f438be11460523
LI3L <khtkvg@gmail.com>
2024-06-21 01:14:01 +0300

    a

--- app/src/main/java/com/example/foodorderer/Activity/IntroActivity.java
+++ app/src/main/java/com/example/foodorderer/Activity/IntroActivity.java
@@ -3,6 +3,7 @@ package com.example.foodorderer.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,24 +17,32 @@ import com.example.foodorderer.databinding.ActivityIntroBinding;
 import java.util.zip.Inflater;
 
 public class IntroActivity extends AppCompatActivity {
-    private ActivityIntroBinding binding;
+//    private ActivityIntroBinding binding;
+    private Button startBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivityIntroBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+//        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_intro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        binding.startBtn.setOnClickListener(new View.OnClickListener() {
+        startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(IntroActivity.this,MainActivity.class));
             }
         });
+//        binding.startBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+//            }
+//        });
     }
 }
--- app/src/main/java/com/example/foodorderer/Activity/MainActivity.java
+++ app/src/main/java/com/example/foodorderer/Activity/MainActivity.java
@@ -7,10 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodorderer.Adaptor.CategoryAdaptor;
+import com.example.foodorderer.Domain.CategoryDomain;
 import com.example.foodorderer.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategorieyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerViewCategoriey();
+    }
+
+    private void recyclerViewCategoriey() {
+        LinearLayoutManager LinearLaoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategorieyList = findViewById(R.id.recyclerViewCategorieyList);
+        recyclerViewCategorieyList.setLayoutManager(LinearLaoutManager);
+
+        ArrayList<CategoryDomain> category = new ArrayList<>();
+        category.add(new CategoryDomain("Pizza", "cat_1"));
+        category.add(new CategoryDomain("Burger", "cat_2"));
+        category.add(new CategoryDomain("Hot dog", "cat_3"));
+        category.add(new CategoryDomain("Drink", "cat_4"));
+        category.add(new CategoryDomain("Donut", "cat_5"));
+
+        adapter = new CategoryAdaptor(category);
     }
 }
