3ed44a3b41527f4e2af2dbf29c9137e87038cac9
LI3L <85995551+LI3L@users.noreply.github.com>
2024-06-20 11:59:30 +0300

    a

--- app/src/main/java/com/example/foodorderer/Activity/IntroActivity.java
+++ app/src/main/java/com/example/foodorderer/Activity/IntroActivity.java
@@ -1,6 +1,8 @@
 package com.example.foodorderer.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +11,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.foodorderer.R;
+import com.example.foodorderer.databinding.ActivityIntroBinding;
+
+import java.util.zip.Inflater;
 
 public class IntroActivity extends AppCompatActivity {
-
+    private ActivityIntroBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_intro);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
         });
     }
 }
