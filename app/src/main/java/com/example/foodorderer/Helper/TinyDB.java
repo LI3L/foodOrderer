c9722b56355f6503ce7851ab2666e496bc4f3d79
LI3L <khtkvg@gmail.com>
2024-06-23 14:37:17 +0300

    a

--- app/src/main/java/com/example/foodorderer/Helper/TinyDB.java
+++ app/src/main/java/com/example/foodorderer/Helper/TinyDB.java
@@ -65,7 +65,6 @@ public class TinyDB {
             bitmapFromPath = BitmapFactory.decodeFile(path);
 
         } catch (Exception e) {
-            // TODO: handle exception
             e.printStackTrace();
         }
 
